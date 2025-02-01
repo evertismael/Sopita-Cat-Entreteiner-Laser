d6dd8a81b23b86b602ec7426c432425a62af7164
evertismael <evertismael@gmail.com>
2025-02-01 08:10:12 +0100

    modifying two servos calibration

--- libs/myservo.py
+++ libs/myservo.py
@@ -5,10 +5,19 @@ MIN_VALUE = -1
 
 def manual_left_right(ch:str, servo:Servo, step:float):
 
-    if ch==81: # use right arrrow
+    if ch==81: # use left arrrow
         servo.value = min([servo.value + step, MAX_VALUE])
-        print('right')
-    elif ch==83: # use left arrow
-        servo.value = max([servo.value - step, MIN_VALUE])
         print('left')
+    elif ch==83: # use right arrow
+        servo.value = max([servo.value - step, MIN_VALUE])
+        print('right')
+    return
+
+def manual_up_down(ch:str, servo:Servo, step:float):
+    if ch==82: # use up arrrow
+        servo.value = min([servo.value + step, MAX_VALUE])
+        print('up')
+    elif ch==84: # use down arrow
+        servo.value = max([servo.value - step, MIN_VALUE])
+        print('down')
     return
