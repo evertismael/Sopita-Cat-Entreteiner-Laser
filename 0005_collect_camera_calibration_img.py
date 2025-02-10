d81cde10aaaf36a879acab3e10c8c09a7c6dfd0c
Evert Pocoma <evertismael@gmail.com>
2025-02-10 14:00:43 +0100

    camera calibration: collection of images

---
+++ 0005_collect_camera_calibration_img.py
@@ -0,0 +1,27 @@
+import cv2
+import time
+
+cam = cv2.VideoCapture(0)
+cam.set(cv2.CAP_PROP_BUFFERSIZE,1)
+a=cam.get(cv2.CAP_PROP_BUFFERSIZE) 
+
+
+cal_imgs_path = './cal_imgs/'
+
+idx = 0
+while True:
+    ret, frame = cv2.read()
+    cv2.imshow('cam',frame)
+
+    pressed_key = cv2.waitKey(1)
+    if pressed_key==ord('q'):
+        break
+    elif pressed_key==ord('c'):
+        print('capturing image')
+        filename = cal_imgs_path+'/'+str(idx)+'.png'
+        cv2.imwrite(filename, frame)
+        print(f'image {filename} save')
+        time.sleep(1)
+
+cam.release()
+cv2.destroyAllWindows()
