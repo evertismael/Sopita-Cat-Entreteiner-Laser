4581c2d669f2e421622f5a9f449cd1388a0b731f
Evert Pocoma <evertismael@gmail.com>
2025-02-11 11:04:51 +0100

    laser: segment only the black squares from the chessboard

--- 0045_laser_calibrate.py
+++ 0045_laser_calibrate.py
@@ -10,44 +10,96 @@ with open(srv_file) as f:
     srvs_data = f.read()
 
 
-
-def chess_roi_mask(frame, P_pxl, ptrn_size):
-    # find up-left, up-right as well as down-left down-right.
-    chss_corners = P_pxl[:,[0, ptrn_size[0]-1, ptrn_size[0]*(ptrn_size[1]-1), ptrn_size[0]*ptrn_size[1]-1]]
-    chss_corners = chss_corners.astype(int)
+def ind2sub(ptrn_size, sqr_idx):
+    N,_ = ptrn_size
+    u = int(sqr_idx/(N-1))
+    v = int(sqr_idx%(N-1))
+    return u,v
+def corner_indexes(ptrn_size, sqr_idx):
+    '''
+    Return the indexes of the points that are the corners of the square with index sqr_idx.
+    The returned indexes idxs are in the clockwise direction.
+    Example:
+            for sqr_idx=0 (first index), idxs=[0,1,9,8] for a ptrn_size = (8,4)
+    '''
+    N,_ = ptrn_size
+    u,v = ind2sub(ptrn_size, sqr_idx)
     
-    for c_idx in range(chss_corners.shape[1]):
-        cv2.circle(frame,chss_corners[:,c_idx],4,(0,0,0),1,1,0)
+    idxs = []
+    idxs.append(sqr_idx+u     )
+    idxs.append(sqr_idx+u   +1)
+    idxs.append(sqr_idx+u+N +1)
+    idxs.append(sqr_idx+u+N   )
+    return idxs
+
+
+def get_black_sqr_idxs(ptrn_size,first_black_idx):
+    '''
+    Returns the indexes of the black squares. Consider the following pattern:
+        b w b w
+        w b w b    
+    The output is [0,2,5,7]
+    Whereas: 
+        w b w b
+        b w b w
+    The output is [1,3,4,6]
+
+
+    '''
+    sqr_idx_list = np.array([]).astype(int)
+    for v in range(ptrn_size[1]-1):
+        new_idx_list = np.array(range(first_black_idx,(ptrn_size[0]-1),2)).astype(int) + int(v*(ptrn_size[0]-1))
+        sqr_idx_list = np.concatenate((sqr_idx_list,new_idx_list), axis=0)
+
+        if (ptrn_size[0]%2-1)==0:
+            first_black_idx+=1
+            first_black_idx = first_black_idx%2
+
+    return sqr_idx_list
+
+
+
+def get_chess_black_squares(frame, P_pxl, ptrn_size, erode_flag=False):
+    '''
+    Return a mask that only contains the black squares from the chessboard.
     
-    # make mask:
-    xymin = np.min(chss_corners,axis=1)-10*np.ones((2,)).astype(int)
-    xymax = np.max(chss_corners,axis=1)+10*np.ones((2,)).astype(int)
-    mask = np.zeros(frame.shape[:2], dtype="uint8")
-    cv2.rectangle(mask, xymin, xymax, 255, -1)
-    frm = cv2.bitwise_and(frame, frame, mask=mask)
+    inputs: 
+            frame: gray scaled image.
 
-    # threshold the image to find only the black rectangles.
-    thr = 175
-    mask = cv2.inRange(frm, np.array([0]), np.array([thr])) 
+    '''
+    # Step 1: Determine if the pattern starts with a black or white square.
+    idx1 = corner_indexes(ptrn_size, 0)
+    sqr1_pnts = P_pxl[:,idx1].astype(int)
+    mask_sqr1 = np.zeros(frame.shape[:2],dtype="uint8")
+    cv2.fillPoly(mask_sqr1,[sqr1_pnts.T],(255))
+
+    idx2 = corner_indexes(ptrn_size, 1)
+    sqr2_pnts = P_pxl[:,idx2].astype(int)
+    mask_sqr2 = np.zeros(frame.shape[:2],dtype="uint8")
+    cv2.fillPoly(mask_sqr2,[sqr2_pnts.T],(255))
     
-    # invert the mask and do a dilation to remove the white lines that are not yet removed.
-    mask_inv = 255-mask
-    kernel = np.ones((4,4),np.uint8)
-    mask_inv = cv2.dilate(mask_inv,kernel,iterations = 1)
-    mask = 255-mask_inv
-    frm_black = cv2.bitwise_and(frm, frm,mask=mask)
+    # compute the intensity value I: (Lowest 'I' identifies the black square)
+    I_sqr1 = np.sum(cv2.bitwise_and(frame,frame,mask=mask_sqr1))
+    I_sqr2 = np.sum(cv2.bitwise_and(frame,frame,mask=mask_sqr2))
+    
+    first_black_idx = 0 if I_sqr1<I_sqr2 else 1
 
-    # now remove the black parts: after this point only the beam is shown.
-    thr = 135
-    mask = cv2.inRange(frm_black, np.array([thr]), np.array([255])) 
-    for c_idx in range(chss_corners.shape[1]):
-        cv2.circle(mask,chss_corners[:,c_idx],4,(255,255,255),1,1,0)
+    # Step 2: Recover the corner indexes for all squares in the chessboard that are black.
+    sqr_idx_list = get_black_sqr_idxs(ptrn_size,first_black_idx)
 
-    cv2.imshow('', frame)
-    cv2.imshow('m', mask)
-    cv2.waitKey(0)
-    a=2
-    return 2
+    final_mask = np.zeros(frame.shape[:2],dtype="uint8")
+    for sqr_idx in list(sqr_idx_list):
+        idxs = corner_indexes(ptrn_size, sqr_idx)
+        sqr_pnts = P_pxl[:,idxs].astype(int)
+        cv2.fillPoly(final_mask,[sqr_pnts.T],(255))
+
+    # Step 3: erode to reduce the white spaces:
+    kernel = np.ones((7,7),np.uint8)
+    final_mask = cv2.erode(final_mask,kernel,iterations = 1)
+
+    frame_black_sqrs = cv2.bitwise_and(frame,frame,mask=final_mask)  
+    
+    return frame_black_sqrs, final_mask
 
 # Each row in the file contain the name of the image and the angles of the servos.
 # Notice that only one point is recovered per row (Since we have a single beam).
@@ -67,9 +119,14 @@ for row in srvs_data:
     frame_grey = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     ptrn_size = ((11,7))
     P_chs_list, P_pxl_list,img_size = ccu.find_sequence_chessboard_points([img_name], ptrn_size,False)
-    frame_roi = chess_roi_mask(frame_grey, P_pxl_list[0].T, ptrn_size)
-
-
+    fr_sqr_black, roi_mask = get_chess_black_squares(frame_grey, P_pxl_list[0].T, ptrn_size)
+    
+    #cv2.imshow('', frame)
+    #cv2.imshow('a', final_mask)
+    cv2.imshow('dd', fr_sqr_black)
+    cv2.waitKey(0)
+    a=2
+    a=2
 
     #for i in range(300):
 
